8043ec309f6ab645660e84e074e0aa6b6e716928
Sam Moore <sam@betterment.com>
2019-09-09 17:03:04 -0400

    adds support for string updates on relations
    this uses a hacky regexp matching strategy that allows word boundary
    characters except single quotes because those imply that the contents
    are part of a value and not a column name

--- lib/journaled/relation_change_protection.rb
+++ lib/journaled/relation_change_protection.rb
@@ -1,7 +1,15 @@
 module Journaled::RelationChangeProtection
   def update_all(updates, force: false)
     unless force || !@klass.respond_to?(:journaled_attribute_names) || @klass.journaled_attribute_names.empty?
-      conflicting_journaled_attribute_names = @klass.journaled_attribute_names & updates.keys.map(&:to_sym)
+      conflicting_journaled_attribute_names = if updates.is_a?(Hash)
+                                                @klass.journaled_attribute_names & updates.keys.map(&:to_sym)
+                                              elsif updates.is_a?(String)
+                                                @klass.journaled_attribute_names.select do |a|
+                                                  updates.match?(/(^|\A|[ \.\\\"])#{a}[\z \\\"]/)
+                                                end
+                                              else
+                                                raise "unsupported type '#{updates.class}' for 'updates'"
+                                              end
       raise(<<~ERROR) if conflicting_journaled_attribute_names.present?
         .update_all aborted by Journaled::Changes due to journaled attributes:
 
