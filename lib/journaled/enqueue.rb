303f2dea642a450519f934daf6163f5c915f3952
smudge <nathan@betterment.com>
2019-10-28 13:20:40 -0400

    Method rename for clarity

--- lib/journaled/enqueue.rb
+++ lib/journaled/enqueue.rb
@@ -6,12 +6,12 @@ module Journaled
 
     def on_enqueue(&block)
       @on_enqueue = block if block_given?
-      @on_enqueue || delayed_job
+      @on_enqueue || delayed_job_enqueue
     end
 
     private
 
-    def delayed_job
+    def delayed_job_enqueue
       ->(performable) do
         Delayed::Job.enqueue(performable, priority: Journaled.job_priority)
       end
