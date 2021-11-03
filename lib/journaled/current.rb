6096a6ce3b7367884a75fda5751e805e7e4e3b58
smudge <nathan@ngriffith.com>
2021-11-03 16:18:24 -0400

    use Thread.current becasue RequestStore may not be set up in all consuming apps

--- lib/journaled/current.rb
+++ lib/journaled/current.rb
@@ -3,11 +3,11 @@ require 'request_store'
 module Journaled
   module Current
     def self.tags
-      RequestStore.store[:journaled_tags] ||= {}.freeze
+      Thread.current[:journaled_tags] ||= {}.freeze
     end
 
     def self.tags=(value)
-      RequestStore.store[:journaled_tags] = value.freeze
+      Thread.current[:journaled_tags] = value.freeze
     end
 
     def self.actor
