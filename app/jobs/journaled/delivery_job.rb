aa5496b039a941e445494fc90c331fe7a9eca777
smudge <nathan@ngriffith.com>
2021-11-04 12:37:01 -0400

    Revert "Remove last of the legacy app_name support (post-5.0 bump)"
    This reverts commit 39d1bb684f1edbdc1effc1bd80a97301b63fc56d.

--- app/jobs/journaled/delivery_job.rb
+++ app/jobs/journaled/delivery_job.rb
@@ -12,12 +12,26 @@ module Journaled
       raise KinesisTemporaryFailure
     end
 
-    def perform(serialized_event:, partition_key:, stream_name:)
+    UNSPECIFIED = Object.new
+    private_constant :UNSPECIFIED
+
+    def perform(serialized_event:, partition_key:, stream_name: UNSPECIFIED, app_name: UNSPECIFIED)
       @serialized_event = serialized_event
       @partition_key = partition_key
-      @stream_name = stream_name
+      if app_name != UNSPECIFIED
+        @stream_name = self.class.legacy_computed_stream_name(app_name: app_name)
+      elsif stream_name != UNSPECIFIED && !stream_name.nil?
+        @stream_name = stream_name
+      else
+        raise(ArgumentError, 'missing keyword: stream_name')
+      end
 
       journal!
+    end
+
+    def self.legacy_computed_stream_name(app_name:)
+      env_var_name = [app_name&.upcase, 'JOURNALED_STREAM_NAME'].compact.join('_')
+      ENV.fetch(env_var_name)
     end
 
     def kinesis_client_config
