7e3612182bd60765f63f22058524def34d750b44
smudge <nathan@ngriffith.com>
2022-08-16 18:53:43 -0400

    Emit a 'batch' enqueue AS notification

--- app/models/journaled/writer.rb
+++ app/models/journaled/writer.rb
@@ -35,12 +35,12 @@ class Journaled::Writer
   end
 
   def self.enqueue!(*events)
-    events.group_by(&:journaled_enqueue_opts).each do |enqueue_opts, delivery_events|
+    events.group_by(&:journaled_enqueue_opts).each do |enqueue_opts, batch|
       job_opts = enqueue_opts.reverse_merge(priority: Journaled.job_priority)
-      Journaled::DeliveryJob.set(job_opts).perform_later(*delivery_perform_args(delivery_events))
+      ActiveSupport::Notifications.instrument('journaled.batch.enqueue', batch: batch, **job_opts) do
+        Journaled::DeliveryJob.set(job_opts).perform_later(*delivery_perform_args(batch))
 
-      delivery_events.each do |event|
-        ActiveSupport::Notifications.instrument('journaled.event.enqueue', event: event, priority: job_opts[:priority])
+        batch.each { |event| ActiveSupport::Notifications.instrument('journaled.event.enqueue', event: event, **job_opts) }
       end
     end
   end
