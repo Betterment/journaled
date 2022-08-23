fcd82cdb1d6440d8b093ea6910a7fa13ac7dad51
smudge <nathan@ngriffith.com>
2022-08-23 13:40:16 -0400

    Big simplification, rely on @run_commit_callbacks directly

--- lib/journaled/connection.rb
+++ lib/journaled/connection.rb
@@ -2,21 +2,21 @@ module Journaled
   module Connection
     class << self
       def available?
-        Journaled.transactional_batching_enabled && transaction_joinable?
+        Journaled.transactional_batching_enabled && transaction_open?
       end
 
       def stage!(event)
-        raise TransactionSafetyError, <<~MSG unless transaction_joinable?
+        raise TransactionSafetyError, <<~MSG unless transaction_open?
           Transaction not available! By default, journaled event batching requires an open database transaction.
         MSG
 
-        connection.current_transaction._journaled_transaction_handler.staged_events << event
+        connection.current_transaction._journaled_staged_events << event
       end
 
       private
 
-      def transaction_joinable?
-        connection.transaction_open? && connection.current_transaction._journaled_transaction_handler.joinable?
+      def transaction_open?
+        connection.transaction_open?
       end
 
       def connection
@@ -35,7 +35,7 @@ module Journaled
     end
 
     module TestOnlyBehaviors
-      def transaction_joinable?
+      def transaction_open?
         # Transactional fixtures wrap all tests in an outer, non-joinable transaction:
         super && (connection.open_transactions > 1 || connection.current_transaction.joinable?)
       end
--- lib/journaled/transaction_ext.rb
+++ lib/journaled/transaction_ext.rb
@@ -3,80 +3,18 @@ require 'active_record/connection_adapters/abstract/transaction'
 module Journaled
   module TransactionExt
     def before_commit_records
-      @_journaled_committing_records = true
+      super.tap do
+        Writer.enqueue!(*_journaled_staged_events) if @run_commit_callbacks
+      end
+    end
+
+    def commit_records
+      connection.current_transaction._journaled_staged_events.push(*_journaled_staged_events) unless @run_commit_callbacks
       super
     end
 
-    def _journaled_transaction_handler
-      if @_journaled_transaction_handler&.active?
-        @_journaled_transaction_handler
-      else
-        @_journaled_transaction_handler = TransactionHandler.new(txn: self).tap do |txn_handler|
-          txn_handler.joinable = !@_journaled_committing_records
-        end
-      end
-    end
-
-    class TransactionHandler
-      attr_writer :joinable
-
-      def initialize(txn:)
-        raise TransactionSafetyError, <<~MSG unless txn.connection.transaction_open?
-          Transaction not open! By default, journaled event batching requires an open database transaction.
-        MSG
-
-        txn.add_record(self)
-        @txn = txn
-        self.joinable = true
-        @active = true
-      end
-
-      def active?
-        @active
-      end
-
-      def joinable?
-        @joinable
-      end
-
-      def staged_events
-        @staged_events ||= []
-      end
-
-      # The following methods adhere to the API contract defined by:
-      # https://github.com/rails/rails/blob/v6.0.4.7/activerecord/lib/active_record/transactions.rb
-      #
-      # This allows our TransactionHandler to act as a "transaction record" and
-      # run callbacks before/after commit (or after rollback).
-      def before_committed!
-        Writer.enqueue!(*staged_events)
-        @joinable = false
-      end
-
-      def committed!(*)
-        @active = false
-      end
-
-      def rolledback!(*)
-        @joinable = false
-        @active = false
-      end
-
-      if Rails::VERSION::MAJOR < 6
-        # With Rails 6.0, this method is no longer necessary, as its behavior was inlined here:
-        # https://github.com/rails/rails/blob/6-0-stable/activerecord/lib/active_record/connection_adapters/abstract/transaction.rb#L130
-        def add_to_transaction
-          @txn.connection.add_transaction_record(self)
-        end
-      end
-
-      def trigger_transactional_callbacks?
-        true
-      end
-
-      def has_transactional_callbacks?
-        true
-      end
+    def _journaled_staged_events
+      @_journaled_staged_events ||= []
     end
   end
 end
