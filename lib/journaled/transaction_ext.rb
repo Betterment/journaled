2115300b79b388d433357ae14bed1f283a701cdd
smudge <nathan@ngriffith.com>
2022-08-23 13:50:38 -0400

    Safety check for @run_commit_callbacks variable

--- lib/journaled/transaction_ext.rb
+++ lib/journaled/transaction_ext.rb
@@ -2,6 +2,17 @@ require 'active_record/connection_adapters/abstract/transaction'
 
 module Journaled
   module TransactionExt
+    def initialize(*, **)
+      super.tap do
+        raise TransactionSafetyError, <<~MSG unless instance_variable_defined?(:@run_commit_callbacks)
+          Journaled::TransactionExt expects @run_commit_callbacks to be defined on Transaction!
+          This is an internal API that may have changed in a recent Rails release.
+          If you were not expecting to see this error, please file an issue here:
+          https://github.com/Betterment/journaled/issues
+        MSG
+      end
+    end
+
     def before_commit_records
       super.tap do
         Writer.enqueue!(*_journaled_staged_events) if @run_commit_callbacks
