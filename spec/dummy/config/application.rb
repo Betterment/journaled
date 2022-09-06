1391dede98a3798924b83b4c9478d27da7e440d1
smudge <nathan@ngriffith.com>
2022-09-06 14:40:09 -0400

    Toggle zeitwerk on when testing against newer Rails

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,6 +10,7 @@ require "journaled"
 
 module Dummy
   class Application < Rails::Application
+    config.autoloader = Rails::VERSION::MAJOR >= 7 ? :zeitwerk : :classic
     config.active_record.sqlite3.represent_boolean_as_integer = true if Rails::VERSION::MAJOR < 6
     config.active_record.legacy_connection_handling = false if Rails::VERSION::MAJOR >= 7
   end
