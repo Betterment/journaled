ddbc8f9462a4ef9584872c03ca795ac33250bdfc
Ray Zane <raymondzane@gmail.com>
2025-01-22 10:29:33 -0500

    Be a little more specific

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -18,7 +18,9 @@ module Dummy
     config.filter_parameters += [:password]
     config.active_support.deprecation = :raise
 
-    if ActiveJob.gem_version < Gem::Version.new('8.0')
+    # This configuration only applies to Rails 7.2, which is the only version
+    # that will enqueue after commit by default when using the `:test` adapter.
+    if Gem::Requirement.new('~> 7.2.0').satisfied_by?(ActiveJob.gem_version)
       config.active_job.enqueue_after_transaction_commit = :never
     end
   end
